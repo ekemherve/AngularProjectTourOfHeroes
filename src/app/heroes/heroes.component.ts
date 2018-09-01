468d98acc2c6fcecf07b958b2eaaba41e7886793
Ekem herve <ekemherve@gmail.com>
2018-09-01 14:30:37 +0200

    create mock-up and use it

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,10 +9,13 @@ import { Hero } from '../hero';
 export class HeroesComponent implements OnInit {
 
 
-  hero: Hero = {
+  /*hero: Hero = {
     id: 1,
     name: 'Windstorm'
-  };
+  };*/
+
+  heroes = HEROES;
+
 
   constructor() { }
 
